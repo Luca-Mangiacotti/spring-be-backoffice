8b4a2724337797637afadf8d034bbded8075ad7f
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-07-03 13:28:32 +0200

    init project

---
+++ src/main/java/org/lessons/java/spring_be_backoffice/SpringBeBackofficeApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_be_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/spring_be_backoffice/SpringBeBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_be_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
